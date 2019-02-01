8c605c2b9ce0c55800ca2592cccb34f7f5ebb56d
Pedro Pinto <44075417+PedroPinto23@users.noreply.github.com>
2019-02-01 13:51:56 -0200

    Initial commit

---
+++ src/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+function Header() {
+  return (
+    <header>
+      <img
+        src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRnKnZr0XgRqe2vEL93QcTpR8c7MDV-s1Sp8ri2Kipoo9VxtwJwkA"
+        alt="Problem?"
+      />
+      <p> MEME GENERATOR </p>
+    </header>
+  );
+}
+export default Header;
